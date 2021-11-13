cbda85bab5d04be203d84d750159b00ff7d1f80c
etcheverrypablol <etcheverrypablol@gmail.com>
2021-11-13 19:49:40 -0300

    Using object array

--- my-app/src/components/Lists.jsx
+++ my-app/src/components/Lists.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 
 const Lists = () => {
-  const initialState = [1, 2, 3, 4, 5];
+  const initialState = [
+    { id: 1, text: 'Task 1' },
+    { id: 2, text: 'Task 2' },
+    { id: 3, text: 'Task 3' },
+  ];
   const [list, setList] = useState(initialState);
 
   return (
     <div>
       <h2>Lists</h2>
       {list.map((elemnt, index) => (
-        <h4 key={index}>{elemnt}</h4>
+        <h4 key={index}>{elemnt.text}</h4>
       ))}
     </div>
   );
