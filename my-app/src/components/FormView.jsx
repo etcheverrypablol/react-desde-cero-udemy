25571876c9192ef79d5ec6883fafae24f80ef2d6
etcheverrypablol <etcheverrypablol@gmail.com>
2021-11-13 21:46:18 -0300

    Adding useState to inputs

--- my-app/src/components/FormView.jsx
+++ my-app/src/components/FormView.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const FormView = () => {
+  const [fruit, setFruit] = useState('');
+  const [description, setDescription] = useState('');
   return (
     <div>
       <h2>Form</h2>
       <form>
-        <input type='text' placeholder='Insert fruit' className='form-control mb-2' />
-        <input type='text' placeholder='Insert description' className='form-control mb-2' />
-        <button className='btn btn-primary '>Add</button>
+        <input
+          type='text'
+          placeholder='Insert fruit'
+          className='form-control mb-2'
+          onChange={(e) => setFruit(e.target.value)}
+        />
+        <input
+          type='text'
+          placeholder='Insert description'
+          className='form-control mb-2'
+          onChange={(e) => setDescription(e.target.value)}
+        />
+
+        <button className='w-100 btn btn-primary '>Add</button>
       </form>
     </div>
   );
