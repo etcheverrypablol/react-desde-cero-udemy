ca44682882071d6fdb31390c02dcb498a8dfaf27
etcheverrypablol <etcheverrypablol@gmail.com>
2021-11-14 09:55:41 -0300

    Using useSate for create a lis of the fruit

--- my-app/src/components/FormView.jsx
+++ my-app/src/components/FormView.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 const FormView = () => {
   const [fruit, setFruit] = useState('');
   const [description, setDescription] = useState('');
+  const [fruitLis, setFruitList] = useState([]);
 
   const saveData = (e) => {
     e.preventDefault();
@@ -15,6 +16,7 @@ const FormView = () => {
       return;
     }
     console.log('Procesing data...' + fruit + description);
+    setFruitList([...fruitLis, { fruit, description }]);
     // Clean the inputs when insert data and press add button
     e.target.reset();
     setFruit('');
@@ -36,8 +38,14 @@ const FormView = () => {
           className='form-control mb-2'
           onChange={(e) => setDescription(e.target.value)}
         />
-
         <button className='w-100 btn btn-primary '>Add</button>
+        <ul>
+          {fruitLis.map((fruit, index) => (
+            <li key={index}>
+              {fruit.fruit} - {fruit.description}
+            </li>
+          ))}
+        </ul>
       </form>
     </div>
   );
